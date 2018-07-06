01e7af06b9b2c5a1f1545f80d8bb441cfbdc5695
nevakee716 <nevakee716@gmail.com>
2018-07-06 12:22:02 +0200

    correct error when there is no button

--- src/cwLayoutNetwork.physics.js
+++ src/cwLayoutNetwork.physics.js
@@ -3,58 +3,58 @@
 
 
 /*global cwAPI, jQuery */
-(function (cwApi, $) {
+(function(cwApi, $) {
     "use strict";
-    if(cwApi && cwApi.cwLayouts && cwApi.cwLayouts.cwLayoutNetwork) {
-      var cwLayoutNetwork = cwApi.cwLayouts.cwLayoutNetwork;
+    if (cwApi && cwApi.cwLayouts && cwApi.cwLayouts.cwLayoutNetwork) {
+        var cwLayoutNetwork = cwApi.cwLayouts.cwLayoutNetwork;
     } else {
-    // constructor
-        var cwLayoutNetwork = function (options, viewSchema) {
+        // constructor
+        var cwLayoutNetwork = function(options, viewSchema) {
             cwApi.extend(this, cwApi.cwLayouts.CwLayout, options, viewSchema); // heritage
             cwApi.registerLayoutForJSActions(this); // execute le applyJavaScript après drawAssociations
             this.construct(options);
         };
     }
 
-    cwLayoutNetwork.prototype.stopPhysics = function (event) {
-        if(this.physics == true) {
+    cwLayoutNetwork.prototype.stopPhysics = function(event) {
+        if (this.physics == true) {
             this.physics = false;
             event.target.innerHTML = "Enable Physics";
-        }
-        else {
+        } else {
             this.physics = true;
             event.target.innerHTML = "Disable Physics";
         }
         this.updatePhysics();
     };
 
-    cwLayoutNetwork.prototype.updatePhysicsButtonTitle = function (button) {
-        if(button === undefined) button = document.getElementById("cwLayoutNetworkButtonsPhysics" + this.nodeID);
-        if(this.physics == false) {
-            button.innerHTML = "Enable Physics";
+    cwLayoutNetwork.prototype.updatePhysicsButtonTitle = function(button) {
+        if (button === undefined) button = document.getElementById("cwLayoutNetworkButtonsPhysics" + this.nodeID);
+        if (button) {
+            if (this.physics == false) {
+                button.innerHTML = "Enable Physics";
+            } else {
+                button.innerHTML = "Disable Physics";
+            }
         }
-        else {
-            button.innerHTML = "Disable Physics";
-        }
+
     };
 
-    cwLayoutNetwork.prototype.updatePhysics = function (state) {
+    cwLayoutNetwork.prototype.updatePhysics = function(state) {
         var changeset = [];
-        if(state !== undefined) {
+        if (state !== undefined) {
             this.physics = state;
             this.updatePhysicsButtonTitle();
         }
-        if(this.physics == true) {
+        if (this.physics == true) {
             this.nodes.forEach(function(node) {
-                node.physics = !(node.cluster); 
+                node.physics = !(node.cluster);
                 changeset.push(node);
             });
-        }
-        else {
+        } else {
             this.nodes.forEach(function(node) {
                 node.physics = false;
                 changeset.push(node);
-                
+
             });
         }
         this.nodes.update(changeset);
